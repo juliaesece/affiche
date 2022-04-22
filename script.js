917f47e77e4b3c32f360d46ad7b557703e706454
Julia <ecojuliase@gmail.com>
2022-04-22 19:41:50 +0200

    ya ni me acuerdo de que ha cambiado hay color random, bold, y buen posicionamiento de las cosas como form y como affiche, y hay mejores espacios en modo vertical pero todavía falta lo que tengo en obsidian

--- script.js
+++ script.js
@@ -12,33 +12,35 @@ $(document).ready(function(){
   var unmovableRows = 0;
   var totalMinRows = 0;
   var modoVertical = false;
+  var rajouterBibliotheque = false;
 
   // console.log("modo vertical")
   // console.log(modoVertical)
 
   // get date pour affiche
   var curr = new Date; // get current date
+  const month = curr.toLocaleString('fr-FR', { month: 'long' });
   var first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
   var sabado = first + 6; // last day is the first day + 6
   var domingo = first + 7; // last day is the first day + 6
 
-  function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-  }
-
-  function formatDate(date) {
-    return [
-      padTo2Digits(date.getDate()),
-      padTo2Digits(date.getMonth() + 1),
-    ].join('/');
-  }
+  // function padTo2Digits(num) {
+  //   return num.toString().padStart(2, '0');
+  // }
+  //
+  // function formatDate(date) {
+  //   return [
+  //     padTo2Digits(date.getDate()),
+  //     padTo2Digits(date.getMonth() + 1),
+  //   ].join('/');
+  // }
 
   //checkboxes functionality
 
   $("#if-samedi").change(function() {
     if(this.checked) {
       $("#jour1").show();
-      $("#samediDate").text(formatDate(new Date(curr.setDate(sabado))))
+      $("#samediDate").text(String(sabado) + " " + month)
       $(".samediChecks").show();
     } else {
       $("#jour1").hide();
@@ -49,7 +51,7 @@ $(document).ready(function(){
     if(this.checked) {
       $("#jour2").show();
       $(".dimChecks").show();
-      $("#dimancheDate").text(formatDate(new Date(curr.setDate(domingo))))
+      $("#dimancheDate").text(String(domingo) + " " + month)
     } else {
       $("#jour2").hide();
       $(".dimChecks").hide();
@@ -62,6 +64,13 @@ $(document).ready(function(){
     } else {
       $("#jour3").hide();
       $(".WEChecks").hide();
+    }
+  });
+
+  $("#if-bibliotheque").change(function() {
+    if(this.checked) {
+      rajouterBibliotheque = true;
+      // bibliothequeCreator(1);
     }
   });
 
@@ -89,8 +98,8 @@ $(document).ready(function(){
   };
 
   function howManyUnmovableRows(nbEvents, nbDes) {
-    console.log("calculating unmovable rows of smth");
-    console.log(nbDes + ~~(nbEvents/2) + ((nbEvents - nbDes) % 2));
+    // console.log("calculating unmovable rows of smth");
+    // console.log(nbDes + ~~(nbEvents/2) + ((nbEvents - nbDes) % 2));
     return (nbDes + ~~(nbEvents/2) + ((nbEvents - nbDes) % 2));
   }
 
@@ -112,10 +121,28 @@ $(document).ready(function(){
     return parseInt(nbDes) + whatsMinRowsSimple(nbEvents-parseInt(nbDes))
   }
 
+  function bold(text){
+    var bold = /\*\*(.*?)\*\*/gm;
+    var html = text.replace(bold, '<strong>$1</strong>');
+    return html;
+}
+
+  function concatJourId (jour, id) {
+    temporaryId = String(jour) + String(id);
+    return temporaryId;
+  }
+
   // CREATE NEW FORMS
 
-  function formCreator(nb, jour) {
+  function formCreator(nb, jour, des) {
     combinedId = String(jour) + String(nb);
+    function buttonAndFin(el, des) {
+      return el + "\
+        <button class='btn btn-light submitEventForm' type='button' id='btn-" + combinedId + "-" + des + "'>Soumettre</button>\
+        <button class='btn btn-light eraseEventForm' type='button' id='el-btn-" + combinedId + "'>Éliminer</button><br><br>\
+      </div>"
+    };
+
     element = "<div id='form" + combinedId + "'>\
       <div class='form-floating'>\
         <input class='form-control' placeholder='Nom de levent' type='text' id='fnom" + combinedId + "' name='fnom" + combinedId + "'>\
@@ -128,40 +155,49 @@ $(document).ready(function(){
       <div class='form-floating'>\
         <input class='form-control' placeholder='Salle' type='text' id='fsalle" + combinedId + "' name='fsalle" + combinedId + "'>\
         <label for='fsalle" + combinedId + "'>Salle</label>\
-      </div>\
-      <button class='btn btn-light submitEventForm' type='button' id='btn-" + combinedId + "-false'>Soumettre</button><br><br>\
-    </div>";
+        </div>";
+        if (des) {
+          element = element + "\
+              <div class='form-floating'>\
+                <textarea class='form-control' placeholder='Description' id='fDes" + combinedId + "' name='fDes" + combinedId + "'></textarea>\
+                <label for='fDes" + combinedId + "'>Description</label>\
+              </div>";
+          element = buttonAndFin(element, des);
+        } else {
+          element = buttonAndFin(element, des);
+        }
     return element;
   }
 
-  function formCreatorDes(nb, jour) {
-    combinedId = String(jour) + String(nb);
-    element = "<div id='form" + combinedId + "'>\
-      <div class='form-floating'>\
-        <input class='form-control' placeholder='Nom de levent' type='text' id='fnom" + combinedId + "' name='fnom" + combinedId + "'>\
-        <label for='fnom" + combinedId + "'>Nom de l\'event</label>\
-      </div>\
-      <div class='form-floating'>\
-        <input class='form-control' placeholder='Horaire' type='text' id='fhoraire" + combinedId + "' name='fhoraire" + combinedId + "'>\
-        <label for='fhoraire" + combinedId + "'>Horaire</label>\
-      </div>\
-      <div class='form-floating'>\
-        <input class='form-control' placeholder='Salle' type='text' id='fsalle" + combinedId + "' name='fsalle" + combinedId + "'>\
-        <label for='fsalle" + combinedId + "'>Salle</label>\
-      </div>\
-      <div class='form-floating'>\
-        <textarea class='form-control' placeholder='Description' id='fDes" + combinedId + "' name='fDes" + combinedId + "'></textarea>\
-        <label for='fDes" + combinedId + "'>Description</label>\
-      </div>\
-      <button class='btn btn-light submitEventForm' type='button' id='btn-" + combinedId + "-true'>Soumettre</button><br><br>\
-    </div>";
-    return element;
+  function bibliothequeCreator(nb){
+    console.log("inside bibliothequeCreator");
+    var jour = 1;
+    var temporaryId = String(jour) + String(nb);
+    var jourPost = "#jour" + String(jour);
+    var post = "#evenementsDeSam";
+    var toInsert = "<div id='evenement" + temporaryId + "' class='des evenement horizontalDescriptionCard evnmtMargin accrossHorizontal samediClass'>\
+    <h2 id='titre" + temporaryId + "' class='titre-d-evenement cardGauche'>📚 Bibliothèque Arthur Rimbaud</h2>\
+    <p class='horaire cardGauche'><img src='https://raw.githubusercontent.com/juliaesece/affiche/d1946d5b78e07109c270fbffa170a1a5decd2406/clock.png' class='iconito'><span id='horaire" + temporaryId + "'> 10h à 13h - 14h à 18h </span></p>\
+    <p class='salle cardGauche'><img src='https://raw.githubusercontent.com/juliaesece/affiche/d1946d5b78e07109c270fbffa170a1a5decd2406/location.png' class='iconito'><span id='salle" + temporaryId + "'> 1e étage </span></p>\
+    <div class='description cardDroite onlyOneRow' id='description" + temporaryId + "'>Coin nature/climat</div>\
+    </div>\
+    ";
+    if (nb == 0 && modoVertical == true) {
+      console.log("appending bc modo vertical true and first element")
+      $(post).append(toInsert);
+    } else {
+      console.log("after bc modo vertical false o dps 1")
+      $(jourPost).after(toInsert);
+    }
   }
 
   // ONCLICK: add new forms
 
   $("#btnNbEvents").click(function(){
     nbEventsSamedi = ifUndefinedZero($("#fNbEventsSamedi").val());
+    if (rajouterBibliotheque == true) {
+      nbEventsSamedi++
+    }
     nbEventsDimanche = ifUndefinedZero($("#fNbEventsDimanche").val());
     nbEventsWE = ifUndefinedZero($("#fNbEventsWE").val());
     nbEventsSamediDes = ifUndefinedZero($("#fNbEventsSamediDes").val());
@@ -175,16 +211,38 @@ $(document).ready(function(){
     console.log("total min rows: " + totalMinRows)
     // crée les forms pour écrire les events sur l'affiche
 
+    //loopThrough creates forms
     function loopThrough(nbEvents, nbEventsDes, jour, domSelector) {
-      eventsDesCounter = nbEventsDes;
+      var eventsNoDesCounter = nbEvents - nbEventsDes;
+      console.log(eventsNoDesCounter);
+      var eventsDesCounter = nbEventsDes;
+      console.log(eventsDesCounter);
+      var isBibliothequeRajoutee = false;
       for(i = parseInt(nbEvents-1); i >= 0 ; i--){
-        if (nbEvents - eventsDesCounter == nbEvents ){
-          var content = formCreator((i), jour);
-          $(domSelector).after(content);
-        } else {
-          var content = formCreatorDes((i), jour);
-          $(domSelector).after(content);
-          eventsDesCounter--
+        var formDiv = $("#form" + String(jour) + String(i));
+        console.log("cycle ", i, "; rajouterBibliotheque es ", rajouterBibliotheque, "; jour es ", jour)
+        if (rajouterBibliotheque == true && jour == 1 && isBibliothequeRajoutee == false) {
+          bibliothequeCreator(i)
+          isBibliothequeRajoutee = true
+        // if there's still events with description, skip create events. when they have all been created, events without description are handled
+      } else if (eventsNoDesCounter > 0){
+          if (formDiv.length) { //if div exists
+            // console.log("form exists, not doing anything (maybe in future update dynamically)")
+          } else {
+            // console.log("form doesn't exist, creating")
+            var content = formCreator((i), jour, false);
+            $(domSelector).after(content);
+            eventsNoDesCounter--
+          }
+        } else { //create events with description
+          if (formDiv.length) { //checks if event exists
+            // console.log("form exists, not doing anything (maybe in future update dynamically)")
+          } else {
+            // console.log("form doesn't exist, creating")
+            var content = formCreator((i), jour, true);
+            $(domSelector).after(content);
+          }
+
         }
       }
     }
@@ -208,10 +266,12 @@ $(document).ready(function(){
   // essayer de déterminer layout: pas expandre quand pas d'espace, pas laisser espaces vides
 
   function setModoVertical() {
-    if (totalMinRows > 4  ){
       console.log("passing to modoVertical true")
+      $("#affiche-wrapper").removeClass("affiche-wrapper-default");
+      $("#affiche-wrapper").addClass("affiche-wrapper-many-events");
+      $("#jours-wrapper").addClass("joursModeVertical");
+      $(".des.evenement").removeClass("horizontalDescriptionCard");
       modoVertical = true;
-    }
   }
 
   function layoutifier(nbEvents, nbDes, selector){
@@ -235,14 +295,11 @@ $(document).ready(function(){
 
     //voir combien d'events sans description il y a
     var eventsNoDes = nbEvents - nbDes;
-    console.log("desde " + selector)
-    console.log("numero de eventos sin descripcion: " + eventsNoDes)
+    // console.log("desde " + selector)
+    // console.log("numero de eventos sin descripcion: " + eventsNoDes)
 
-    if (totalMinRows > 4 ) {
+    if (modoVertical == true ) {
       console.log("Il y a trop d'événements ou trop d'événements avec description: on va passer au mode vertical");
-      modoVertical = true;
-      $("#jours-wrapper").addClass("joursModeVertical");
-      $(".des.evenement").removeClass("horizontalDescriptionCard");
     } else {
       if (eventsNoDes == 1 ) {
         $(selector + ".evenement").addClass("accrossHorizontal");
@@ -281,18 +338,54 @@ $(document).ready(function(){
   }
 
   $("#layout").click(function(){
+    if (totalMinRows > 4 ) {
+      setModoVertical();
+      alert("Il y a trop d'événements ou trop d'événements avec description: on va passer au mode vertical");
+    }
     layoutifier(nbEventsSamedi, nbEventsSamediDes, ".samediClass");
     layoutifier(nbEventsDimanche, nbEventsDimancheDes, ".dimancheClass");
     layoutifier(nbEventsWE, nbEventsWEDes, ".WEClass");
-    if (totalMinRows > 4 ) {
-      alert("Il y a trop d'événements ou trop d'événements avec description: on va passer au mode vertical");
+
+    // console.log("event length", $(".evenement").length)
+    for(i = ($(".evenement").length - 1); i >= 0 ; i--) {
+      if ((i%2) == 0) {
+        // console.log(i)
+        // console.log("pair")
+        // console.log($(".evenement")[i]);
+        selectedEvent = $(".evenement").eq(i)
+        selectedEvent.addClass("jauneBorders")
+      } else {
+        // console.log(i)
+        // console.log("impair")
+        selectedEvent = $(".evenement").eq(i)
+        selectedEvent.addClass("bleuBorders")
+      }
     }
   });
 
+
+function findPosition(content, jour, id, post) {
+  console.log("id is", id)
+  if (id == 0 ) {
+    console.log("if id is 0 is true")
+    $(post).after(content)
+  } else if ($("#evenement" + concatJourId(jour, (id-1))).length){
+    $("#evenement" + concatJourId(jour, (id-1))).after(content)
+  } else if ($("#evenement" + concatJourId(jour, (id-2))).length) {
+    $("#evenement" + concatJourId(jour, (id-2))).after(content)
+  } else if ($("#evenement" + concatJourId(jour, (id+1))).length) {
+    $("#evenement" + concatJourId(jour, (id+1))).before(content)
+  } else {
+    $(post).after(content)
+  }
+}
+
+
+
 // add to affiche from new/added forms
-  function addToAffiche(jour, nb, des) {
-    var temporaryId = String(jour) + String(nb);
-    var nb = parseInt(nb);
+  function addToAffiche(jour, id, des) {
+    var temporaryId = String(jour) + String(id);
+    var nb = parseInt(id);
     var jourPost = "#jour" + String(jour);
 
     if (jour == 1) {
@@ -317,7 +410,8 @@ $(document).ready(function(){
        $("#salle" + temporaryId).text(" " + salle)
        if (des) {
          var description = $("#fDes" + temporaryId).val();
-         $("#description" + temporaryId).text(description)
+         description = bold(description)
+         $("#description" + temporaryId).html(description)
        }
      } else {
        console.log("evenement doesn't exist, creating")
@@ -333,15 +427,11 @@ $(document).ready(function(){
               <h2 id='titre" + temporaryId + "' class='titre-d-evenement cardGauche'> " + titre + " </h2>\
               <p class='horaire cardGauche'><img src='https://raw.githubusercontent.com/juliaesece/affiche/d1946d5b78e07109c270fbffa170a1a5decd2406/clock.png'  class='iconito'><span id='horaire" + temporaryId + "'> " + horaire + " </span></p>\
               <p class='salle cardGauche'><img src='https://raw.githubusercontent.com/juliaesece/affiche/d1946d5b78e07109c270fbffa170a1a5decd2406/location.png' class='iconito'><span id='salle" + temporaryId + "'> " + salle + " </span></p>\
-              <p class='description cardDroite onlyOneRow' id='description" + temporaryId + "'>" + description + "</p>\
+              <div class='description cardDroite onlyOneRow' id='description" + temporaryId + "'>" + bold(description) + "</div>\
             </div>";
        }
 
-       if (des) {
-         $("#btn" + temporaryId + "-false").text("Modifier")
-      } else {
-         $("#btn" + temporaryId + "-true").text("Modifier")
-       }
+       $("#btn-" + temporaryId + "-" + des).text("Modifier")
 
 
        if (nb == 0 && modoVertical == true) {
@@ -349,11 +439,17 @@ $(document).ready(function(){
          $(post).append(toInsert);
        } else {
          // console.log("after bc modo vertical false o dps 1")
-         $(jourPost).after(toInsert);
+         findPosition(toInsert, jour, id, jourPost);
        }
 
      }
   };
+
+  function eliminer(jour, nb){
+    let combinedId = String(jour) + String(nb)
+    $("#form" + combinedId).remove();
+    $("#evenement" + combinedId).remove();
+  }
 
  $(document).on('click','.submitEventForm', function(){
     console.log("event form clicked");
@@ -364,12 +460,27 @@ $(document).ready(function(){
     let nb = this.id.slice(5, 6)
     // console.log("nb is " + nb)
     let des = this.id.slice(7)
-    // console.log(des)
+    // console.log("premier des", des)
     des = (des === 'true')
     // console.log("des is ")
     // console.log(des)
+    // console.log("add to affiche ", jour, nb, des);
     addToAffiche(jour, nb, des);
   });
+
+$(document).on('click','.eraseEventForm', function(){
+  console.log("clicked erase")
+  console.log("this", this.id)
+  let jour = this.id.slice(7, 8)
+  // console.log("jour is " + jour)
+  let nb = this.id.slice(8, 9)
+  // console.log("nb is " + nb)
+  // console.log(des)
+  // console.log("des is ")
+  // console.log(des)
+  console.log(jour, nb);
+  eliminer(jour, nb);
+});
 
 // plan B pour quand télécharger ne marche pas: imprimir comme si c'était une page
 
@@ -394,33 +505,53 @@ $(document).ready(function(){
     toggleJourText();
   });
 
-  window.onload = function () {
-    document.getElementById("downloadAffiche2")
-        .addEventListener("click", () => {
-            // const affiche = this.document.getElementById("affiche-wrapper");
-            var affiche = "";
-            var download = function(){
-              var canvas = document.getElementById('dCanvas');
-              var anchor = document.createElement("a");
-              anchor.href = canvas.toDataURL("image/png");
-              anchor.download = "affiche.png";
-              anchor.click();
-            }
-            $("img").crossOrigin = "anonymous";
-            html2canvas(document.querySelector("#affiche-wrapper"), {
-              useCORS:true,
-              onrendered: function(canvas) {
-                var result = canvas.toDataURL();
-              }
-            }).then(canvas => {
-                canvas.setAttribute("id", "dCanvas")
-                document.body.appendChild(canvas)
-                affiche = canvas;
-                // console.log("affiche is")
-                // console.log(affiche)
-                download();
-            });
-          });
-}
+  // var createPdf = function () {
+    // var ctx = new canvas2pdf.PdfContext(blobStream());
+    // var text = editor.getValue();
+    // var stream = blobStream();
+    // var ctx = new canvas2pdf.PdfContext(stream);
+    // eval(text);
+    // var createPdf = function (stream) {
+    //   var ctx = new canvas2pdf.PdfContext(stream);
+    //   ctxeado = ctx.toBlobURL('application/pdf')
+      // ctx.stream.on('finish', function () {
+      //     iframe.src = ctx.stream.toBlobURL('application/pdf');
+      //   });
+      // };
+    // };
+
+  $(document).on('click','#downloadAffiche2', function () {
+      // const affiche = this.document.getElementById("affiche-wrapper");
+      console.log("telecharging starting")
+      var affiche = "";
+
+      var download = function(){
+        var canvas = document.getElementById('dCanvas');
+        // var ctx = new canvas2pdf.PdfContext(canvas); quitar esto pa volver a la normalidad
+        var anchor = document.createElement("a");
+        anchor.href = canvas.toDataURL("image/png");
+        // anchor.href = ctx.toBlobURL('application/pdf');
+        anchor.download = "affiche-WE.pdf";
+        anchor.click();
+      }
+
+      $("img").crossOrigin = "anonymous";
+
+      html2canvas(document.querySelector("#affiche-wrapper"), {
+        useCORS: true,
+        onrendered: function(canvas) {
+          var result = canvas.toDataURL();
+        }
+      }).then(canvas => {
+          canvas.setAttribute("id", "dCanvas")
+          document.body.appendChild(canvas)
+          affiche = canvas;
+          console.log("affiche is")
+          console.log(affiche)
+          download();
+      });
+    });
+
+
 
 });
